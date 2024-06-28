4ad20577bac164c50bae726a4e3d89102fe5fbdd
Ghaith-oss <128695767+Ghaith-oss@users.noreply.github.com>
2024-06-28 10:17:38 +0200

    PlayTest

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,14 @@ import { check } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '2m', target: 10 }, // Ramp up to 10 users over 2 minutes
-    { duration: '3m', target: 10 }, // Stay at 10 users for 3 minutes
-    { duration: '2m', target: 0 },  // Ramp down to 0 users over 2 minutes
+    { duration: '2m', target: 100 }, // Ramp up to 10 users over 2 minutes
+    { duration: '2m', target: 500 }, // Stay at 10 users for 3 minutes
+    { duration: '1m', target: 50 },  // Ramp down to 0 users over 2 minutes
   ],
 };
 
 const BASE_URL = 'https://l0gmn7llz8.execute-api.eu-north-1.amazonaws.com/dev';
-const AUTH_TOKEN = 'your-auth-token'; // Replace with your actual auth token
+const AUTH_TOKEN = 'eyJraWQiOiJFd0xhRE45MDBYeWVyNjBxQlU4U3g3Mk9TcFVBeWd2OUx2NkpGUHRHaUljPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJkMDljMDk4Yy1mMGYxLTcwODItYzliNy1mMjA2NTMwNTIzMTMiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmV1LW5vcnRoLTEuYW1hem9uYXdzLmNvbVwvZXUtbm9ydGgtMV9ibzRDOFlWNzIiLCJjb2duaXRvOnVzZXJuYW1lIjoidXNlcnRlc3QiLCJvcmlnaW5fanRpIjoiYjE2ZTYzNzktYjA2NC00MWE5LWI5YmEtNWU1MTQ2N2I4YTM2IiwiYXVkIjoiM3VlY2prbjY5YW4yaTdiZ3VyMjN0NXBtdTIiLCJldmVudF9pZCI6IjUwNTJhOTAwLWI2ZTMtNDNhMy04YmI4LTljZmUxNTM5MzViZSIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNzE5NDAyMDA4LCJleHAiOjE3MTk1NjUyMDAsImlhdCI6MTcxOTU2MTYwMCwianRpIjoiNGJkMzM3YTAtYzE5NC00YjBhLWJiNzgtMDE0MWZkN2FjYzRjIiwiZW1haWwiOiJnaGFpdGhhbG5hamphcjRAZ21haWwuY29tIn0.oRwqbjbhXYZO9id3HtgVkYbzfGnwwvuEZGT4UeMo1XXGI3V8n_MqRiTKwSAk8_hmaajLsb8f8xi9kMp35xsXL9N1QWDRdOU5m7sYZTwr-llrcReeOYOQQG6DzALAfO6EIenjuB7HaUtldHF1-nvQpX_U41Z5A3S-B_db9GVxsR-ObPeJCmRJWv-wyQBYjQWOcfE_fhg5kxuV1UzEKAfT4ZBXmP1Fkw5ecmapDAKNXO-0E_gab5R3JsWyjhgs6uEFqQTNmV0IhIK4rzMqQRJ0s7l4UHmVS0VzXurk0OV13Of2ID-sCdN4bhvAlzWqRkL8ipTAH2s0jINitST6LGIHoA'; // Replace with your actual auth token
 const HEADERS = {
   headers: {
     'auth-token': AUTH_TOKEN,
